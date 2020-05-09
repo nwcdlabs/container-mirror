88ab74fc22645b606fd355a2af0781f6efa6950e
nowfox <nowfox@163.com>
2020-05-09 11:11:53 +0800

    修改profile名称

--- mirror/mirror-images.sh
+++ mirror/mirror-images.sh
@@ -22,7 +22,6 @@ function replaceDomainName(){
   for key in ${!DOMAIN_MAP[*]};do
     if [[ $URI == ${key}* ]]; then
 	  math_mirror=True
-      #URI=`echo ${URI} | sed "s/${key}/${DOMAIN_MAP[$key]}/"`
 	  URI=${URI/#${key}/${DOMAIN_MAP[$key]}}
 	  break
     fi
@@ -38,14 +37,14 @@ function createEcrRepo() {
     echo "repo: $1 already exists"
   else
     echo "creating repo: $1"
-    aws --profile=China --region ${ECR_REGION} ecr create-repository --repository-name "$1"    
+    aws --profile=ChinaECR --region ${ECR_REGION} ecr create-repository --repository-name "$1"    
     attachPolicy "$1"
   fi
 }
 
 function attachPolicy() {
   echo "attaching public-read policy on ECR repo: $1"
-  aws --profile China --region $ECR_REGION ecr set-repository-policy --policy-text file://policy.text --repository-name "$1"
+  aws --profile ChinaECR --region $ECR_REGION ecr set-repository-policy --policy-text file://policy.text --repository-name "$1"
 }
 
 function isRemoteImageExists(){
@@ -53,7 +52,7 @@ function isRemoteImageExists(){
   fullrepo=${1#*/}
   repoName=${fullrepo%%:*}
   tag=${fullrepo##*:}
-  res=$(aws --profile China --region $ECR_REGION ecr describe-images --repository-name "$repoName" --query "imageDetails[?(@.imageDigest=='$2')].contains(@.imageTags, '$tag') | [0]")
+  res=$(aws --profile ChinaECR --region $ECR_REGION ecr describe-images --repository-name "$repoName" --query "imageDetails[?(@.imageDigest=='$2')].contains(@.imageTags, '$tag') | [0]")
 
   if [ "$res" == "true" ]; then 
     return 0 
@@ -81,8 +80,8 @@ function inArray() {
 }
 
 function loginEcr() {
-  aws --profile=China ecr --region cn-northwest-1 get-login --no-include-email | sh
-  #aws --profile=China ecr --region cn-north-1 get-login --no-include-email | sh
+  aws --profile=ChinaECR ecr --region cn-northwest-1 get-login --no-include-email | sh
+  #aws --profile=ChinaECR ecr --region cn-north-1 get-login --no-include-email | sh
   aws ecr get-login --region us-west-2 --registry-ids 602401143452 894847497797 --no-include-email | sh
 }
 
@@ -111,7 +110,7 @@ function pullAndPush(){
 }
 
 # list all existing repos
-allEcrRepos=$(aws --profile=China --region $ECR_REGION ecr describe-repositories --query 'repositories[*].repositoryName' --page-size 1000 --output text)
+allEcrRepos=$(aws --profile=ChinaECR --region $ECR_REGION ecr describe-repositories --query 'repositories[*].repositoryName' --page-size 1000 --output text)
 echo "allEcrRepos:$allEcrRepos"
 repos=$(grep -v ^# $IMAGES_FILE_LIST | cut -d: -f1 | sort -u)
 for repo in ${repos[@]}
